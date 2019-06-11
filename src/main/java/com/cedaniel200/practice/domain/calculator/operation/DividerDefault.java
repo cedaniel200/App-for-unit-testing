e48ee168a169159850d3db8e82b9e21c36571579
cesar daniel <cdanielmg200@gmail.com>
2019-06-11 10:58:58 -0500

    Se corrige error en la clase DividerDefault

--- src/main/java/com/cedaniel200/practice/domain/calculator/operation/DividerDefault.java
+++ src/main/java/com/cedaniel200/practice/domain/calculator/operation/DividerDefault.java
@@ -3,6 +3,6 @@ package com.cedaniel200.practice.domain.calculator.operation;
 public class DividerDefault implements Divider {
     @Override
     public int divide(int dividend, int divider) throws ArithmeticException {
-        return dividend - divider;
+        return dividend / divider;
     }
 }
