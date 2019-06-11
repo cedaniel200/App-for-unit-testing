2fc5f71fa20657cabc898bc45c340c28a525b8ed
cesar daniel <cdanielmg200@gmail.com>
2019-06-11 11:03:15 -0500

    Se deja la clase de pruebas de la calculadora vacía

--- src/test/java/com/cedaniel200/practice/domain/CalculatorDefaultTest.java
+++ src/test/java/com/cedaniel200/practice/domain/CalculatorDefaultTest.java
@@ -1,32 +1,6 @@
 package com.cedaniel200.practice.domain;
 
-import com.cedaniel200.practice.domain.calculator.Calculator;
-import com.cedaniel200.practice.domain.calculator.CalculatorDefault;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 public class CalculatorDefaultTest {
-
-    private Calculator calculator;
-
-    @Before
-    public void setup(){
-        // Arrange
-        calculator = new CalculatorDefault();
-    }
-
-    @Test
-    public void mustBeSuccessfulIfAddPositiveIntegerNumbers(){
-        // Arrange
-        int expected = 10;
-
-        // Act
-        int actual = calculator.add(5, 5);
-
-        // Assert
-        Assert.assertEquals(expected, actual);
-    }
 
     //TODO create the necessary unit testings
 
