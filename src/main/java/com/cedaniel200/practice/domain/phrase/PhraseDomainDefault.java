7ce38a3a46ca7df3d5cc3ac936997d7d9825bb78
cesar daniel <cdanielmg200@gmail.com>
2019-06-14 12:11:03 -0500

    Se agrega un poco mas de logica al PhraseDomainDefault

--- src/main/java/com/cedaniel200/practice/domain/phrase/PhraseDomainDefault.java
+++ src/main/java/com/cedaniel200/practice/domain/phrase/PhraseDomainDefault.java
@@ -5,6 +5,7 @@ import com.cedaniel200.practice.persistence.phrase.PhraseDao;
 
 public class PhraseDomainDefault implements PhraseDomain {
 
+    private static final String DEFAULT_AUTHOR = "anonymous";
     private PhraseDao phraseDao;
 
     public PhraseDomainDefault(PhraseDao phraseDao) {
@@ -13,6 +14,9 @@ public class PhraseDomainDefault implements PhraseDomain {
 
     @Override
     public long create(String phrase, String author) {
+        if(author.equals("")){
+            author = DEFAULT_AUTHOR;
+        }
         Phrase phrase1 = new Phrase(phrase, author);
         return phraseDao.create(phrase1);
     }
