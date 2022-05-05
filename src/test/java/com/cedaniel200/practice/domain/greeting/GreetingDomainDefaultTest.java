8bec5aed33180c7a12c826d0c5774ee7780d87fa
Cesar Daniel Meneses <cdanielmg200@gmail.com>
2022-05-05 10:26:45 -0500

    Se cambian las versiondes de algunas dependencias asi como se agregan unas nuevas para ser compatibles con Java 11, lo que obliga a modificar algunos test

--- src/test/java/com/cedaniel200/practice/domain/greeting/GreetingDomainDefaultTest.java
+++ src/test/java/com/cedaniel200/practice/domain/greeting/GreetingDomainDefaultTest.java
@@ -2,57 +2,44 @@ package com.cedaniel200.practice.domain.greeting;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.junit.runner.RunWith;
-import org.mockito.BDDMockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
-import java.io.File;
-import java.io.IOException;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(GreetingByLanguage.class)
 public class GreetingDomainDefaultTest {
 
-    @Rule
-    public TemporaryFolder temporaryFolder = new TemporaryFolder();
-    private File greetingTxt;
     private GreetingDomain greetingDomain;
 
     @Before
-    public void setup() throws IOException {
-        greetingTxt = temporaryFolder.newFile("greetings.txt");
+    public void setup() {
         greetingDomain = new GreetingDomainDefault();
     }
 
     //metodo static
     @Test
-    public void mustBeSuccessfulIfReturnHola() throws Exception {
-        PowerMockito.mockStatic(GreetingByLanguage.class);
+    public void mustBeSuccessfulIfReturnHola() {
         String expectedGreeting = "Hola";
-        when(GreetingByLanguage.getGreeting(anyString())).thenReturn(expectedGreeting);
+        try (MockedStatic<GreetingByLanguage> greetingByLanguage = Mockito.mockStatic(GreetingByLanguage.class)) {
+            greetingByLanguage.when(() -> GreetingByLanguage.getGreeting("es"))
+                    .thenReturn(expectedGreeting);
 
-        String greeting = greetingDomain.greet("es");
+            String greeting = greetingDomain.greet("es");
 
-        Assert.assertEquals(expectedGreeting, greeting);
+            Assert.assertEquals(expectedGreeting, greeting);
+        }
     }
 
     @Test
-    public void mustBeSuccessfulIfReturnUnsupportedLanguage() throws Exception {
-        PowerMockito.mockStatic(GreetingByLanguage.class);
+    public void mustBeSuccessfulIfReturnUnsupportedLanguage() {
         String expectedGreeting = "unsupported language";
-        BDDMockito.given(GreetingByLanguage.getGreeting("it")).willReturn(expectedGreeting);
+        try (MockedStatic<GreetingByLanguage> greetingByLanguage = Mockito.mockStatic(GreetingByLanguage.class)) {
+            greetingByLanguage.when(() -> GreetingByLanguage.getGreeting("it"))
+                    .thenReturn(expectedGreeting);
 
-        String greeting = greetingDomain.greet("it");
+            String greeting = greetingDomain.greet("it");
 
-        Assert.assertEquals(expectedGreeting, greeting);
+            Assert.assertEquals(expectedGreeting, greeting);
+        }
     }
 
 }
